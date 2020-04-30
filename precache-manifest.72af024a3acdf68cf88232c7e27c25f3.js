cbc442dea32d97a98437a73289a9e350755bbfbf
Abdulhakeem Usman <ibikunleusman@outlook.com>
2020-04-29 21:32:38 -0400

    Updates

---
+++ precache-manifest.72af024a3acdf68cf88232c7e27c25f3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a574dff03946829450e0202c30f8db2c",
+    "url": "/myweatherapp/index.html"
+  },
+  {
+    "revision": "b1d0628b8c2e720957ea",
+    "url": "/myweatherapp/static/css/2.c1e85ec4.chunk.css"
+  },
+  {
+    "revision": "183fa145744b998bb086",
+    "url": "/myweatherapp/static/css/main.c1618807.chunk.css"
+  },
+  {
+    "revision": "b1d0628b8c2e720957ea",
+    "url": "/myweatherapp/static/js/2.6a647c96.chunk.js"
+  },
+  {
+    "revision": "183fa145744b998bb086",
+    "url": "/myweatherapp/static/js/main.41201fc4.chunk.js"
+  },
+  {
+    "revision": "6571059039903a668e2f",
+    "url": "/myweatherapp/static/js/runtime-main.6b4460eb.js"
+  },
+  {
+    "revision": "ce211a99c7ae98dfae7c6f335536d3e8",
+    "url": "/myweatherapp/static/media/night-rain.ce211a99.jpg"
+  },
+  {
+    "revision": "da5dad4ddfbd03bd948248f26db80502",
+    "url": "/myweatherapp/static/media/night.da5dad4d.jpg"
+  },
+  {
+    "revision": "e701603c6223ff0e9364bf836962f917",
+    "url": "/myweatherapp/static/media/rainy.e701603c.jpg"
+  },
+  {
+    "revision": "00660aaaed5b2047025df2fc997a5cce",
+    "url": "/myweatherapp/static/media/snow-night.00660aaa.jpg"
+  },
+  {
+    "revision": "2e68acc03aef2f389db634297f50525a",
+    "url": "/myweatherapp/static/media/snowy.2e68acc0.jpg"
+  },
+  {
+    "revision": "90e77b0dceabda3769cb2b3d1a977c3a",
+    "url": "/myweatherapp/static/media/storm-day.90e77b0d.jpg"
+  },
+  {
+    "revision": "5c4e3693720141a8aa1cf0a534e527fd",
+    "url": "/myweatherapp/static/media/stormy.5c4e3693.jpg"
+  },
+  {
+    "revision": "d4db3b1c7ef05b06204dcc5a7bd7b67e",
+    "url": "/myweatherapp/static/media/sunny.d4db3b1c.jpg"
+  },
+  {
+    "revision": "c881e10ce953c24a3a8bc2998bd45c0a",
+    "url": "/myweatherapp/static/media/sunset.c881e10c.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/myweatherapp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/myweatherapp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/myweatherapp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/myweatherapp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/myweatherapp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
